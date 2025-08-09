bdbab7fb98454fb5f50826c0bae5e752f7afd895
killuaaax <killuarog@gmail.com>
2025-08-09 08:27:56 +0100

    Add files via upload

---
+++ nenshema.js
@@ -0,0 +1,49 @@
+let bgimg = document.getElementById("bg-image")
+function enhancehover() {
+    
+    bgimg.style.backgroundImage='url("images/enhancer-shema.png")'
+}
+function enhanceleave() {
+    
+    bgimg.style.backgroundImage='url("images/nen-shema.JPG")'
+}
+function specialisationhover() {
+    
+    bgimg.style.backgroundImage='url("images/specialisation-shema.JPG")'
+}
+function specialisationleave() {
+    
+    bgimg.style.backgroundImage='url("images/nen-shema.JPG")'
+}
+function emissionhover() {
+    
+    bgimg.style.backgroundImage='url("images/emission-shema.JPG")'
+}
+function emissionleave() {
+    
+    bgimg.style.backgroundImage='url("images/nen-shema.JPG")'
+}
+function transmutationhover() {
+    
+    bgimg.style.backgroundImage='url("images/transmutation-shema.JPG")'
+}
+function transmutationleave() {
+    
+    bgimg.style.backgroundImage='url("images/nen-shema.JPG")'
+}
+function manipulationhover() {
+    
+    bgimg.style.backgroundImage='url("images/manipulation-shema.JPG")'
+}
+function manipulationleave() {
+    
+    bgimg.style.backgroundImage='url("images/nen-shema.JPG")'
+}
+function conjurationhover() {
+    
+    bgimg.style.backgroundImage='url("images/conjuration-shema.JPG")'
+}
+function conjurationleave() {
+    
+    bgimg.style.backgroundImage='url("images/nen-shema.JPG")'
+}
